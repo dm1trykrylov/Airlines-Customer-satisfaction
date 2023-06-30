997eaf2cfbebef190be5919fb26bcc9127f814d2
dm1trykrylov <231014d@gmail.com>
2023-06-30 23:21:03 +0400

    Fix feature encoding

--- model.py
+++ model.py
@@ -7,39 +7,23 @@ PATH_TO_MODEL = 'models/CatBoostClassifier.pickle'
 
 
 def encode_features(df: pd.DataFrame) -> pd.DataFrame:
-    """ 
     encoderfile = 'encoder.sav'
     with open('models/' + encoderfile, 'rb') as f:
         ordinal_encoder = pickle.load(f)
         ordinal_columns = ['Customer Type', 'Class', 'Gender']
         df[ordinal_columns] = ordinal_encoder.transform(df[ordinal_columns])
-    """
-    #df.drop('Gender', axis=1, inplace=True)
-    df_full = pd.read_csv('./datasets/clients.csv')
-    df_full.drop(['id'], axis=1, inplace=True)
-    df_full.dropna(inplace=True)
-    df_full.drop(df_full[df_full['satisfaction'] == '-'].index, inplace=True)
-    ordinal_encoder = OrdinalEncoder()
-    ordinal_columns = ['Customer Type', 'Class', 'Gender']
-    ordinal_encoder = ordinal_encoder.fit(df_full[ordinal_columns])
-    
-    df[ordinal_columns] = ordinal_encoder.transform(df[ordinal_columns])
-    df_full[ordinal_columns] = ordinal_encoder.transform(df_full[ordinal_columns])
-    
     df = pd.get_dummies(data=df, prefix=['Travel'], columns=['Type of Travel'])
-    df_full = pd.get_dummies(data=df_full, prefix=['Travel'], columns=['Type of Travel'])
-    if 'Travel_Business travel' not in df.columns:
+    if not ('Travel_Business travel' in df.columns):
         df['Travel_Business travel'] = [0]
+        swap_columns(df, 'Travel_Business travel', 'Travel_Personal Travel')  
     else:
         df['Travel_Personal Travel'] = [0]
+    st.write(df.head())
     scalerfile = 'scaler.sav'
     with open('models/' + scalerfile, 'rb') as f:
-        print("Success!\n")
-        #scaler = pickle.load(f)
-        #df = pd.DataFrame(scaler.transform(df), columns=df.columns)
-    scaler = MinMaxScaler()
-    scaler.fit(df_full.drop(['satisfaction'], axis=1))
-    df = pd.DataFrame(scaler.transform(df), columns=df.columns)
+        print("S!\n")
+        scaler = pickle.load(f)
+        df = pd.DataFrame(scaler.transform(df), columns=df.columns)
     return df
 
 
@@ -47,13 +31,30 @@ def encode_radio_input(input: str) -> int:
     return 0 if input == "skip" else int(input)
 
 
+def swap_columns(df, col1, col2):
+    """
+    Функция, позволяющая поменять местами два столбца таблицы
+    """
+    # Названия столбцов
+    cols = df.columns
+    
+    # Индексы столбцов, которые нужно поменять
+    col1_idx = cols.get_loc(col1)
+    col2_idx = cols.get_loc(col2)
+    
+    # Перестановка двух столбцов по индексам
+    df[[cols[col1_idx], cols[col2_idx]]] = df[[cols[col2_idx], cols[col1_idx]]]
+    
+    # Переименование столбцов
+    df.rename(columns = {col1:col2, col2:col1}, inplace = True)
+
 def load_model_and_predict(df, path=PATH_TO_MODEL):
     df = encode_features(df)
     st.write(df.head())
     df.to_csv('test.csv')
     with open(path, "rb") as f:
         model = pickle.load(f)
-        #st.write(df.info())
+        # st.write(df.info())
         prediction = model.predict(df)
         # prediction = np.squeeze(prediction)
         prediction_proba = model.predict_proba(df)
